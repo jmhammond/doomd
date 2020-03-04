83d91cf3c328b44a0586995eb480b6253662a8d7
John Hammond <jmhammond@gmail.com>
2020-03-04 11:11:44 -0600

    Added thunderlink hyperlink handling! And cleanup
    Also changed the default theme to nord-light because night mode is hard
    to read sometimes in the evenings (because Blackboard is so bright,
    jumping back and forth... oh man.)

--- config.el
+++ config.el
@@ -2,18 +2,24 @@
 
 ;; Place your private configuration here
 
-;; ui
-;;
-(setq doom-font (font-spec :family "Source Code Pro" :size 28))
+(setq user-full-name "John Hammond"
+      user-mail-address "jmhammond@gmail.com"
 
-(setq doom-theme 'doom-nord)
+      ;; make *Scratch* act like org-mode
+      doom-scratch-buffer-major-mode 'org-mode
 
-;; crostini doesn't let us do top/left, but the width/height works
-(setq initial-frame-alist
-      '((top . 1) (left . 1) (width . 100) (height . 40)))
+      ;; ui
+      doom-font (font-spec :family "Source Code Pro" :size 28)
+      doom-theme 'doom-nord-light
+      +doom-dashboard-banner-file (expand-file-name "coffeesquirrel.png" doom-private-dir)
 
-;; coffee squirrrel for my emacs logo
-(setq +doom-dashboard-banner-file (expand-file-name "coffeesquirrel.png" doom-private-dir))
+      ;; crostini doesn't let us do top/left, but the width/height works
+      initial-frame-alist '((top . 1) (left . 1) (width . 100) (height . 40))
+
+      ;; remove line numbers to speed up emacs
+      display-line-numbers-type nil
+)
+
 
 ;; soft wrap everywhere
 ;; (note I also needed something in init.el)
@@ -44,16 +50,16 @@
 
 ;; .. but this does; the 'compilation-finish-function runs after 'compile
 ;; https://emacs.stackexchange.com/questions/62/hide-compilation-window
-(setq compilation-finish-function
-;; (add-hook! 'compilation-finish-functions  <-- don't know why doom-emacs doesn't like this
-  (lambda (buf str)
-    (if (null (string-match ".*exited abnormally.*" str))
-        ;;no errors, make the compilation window go away in a few seconds
-        (progn
-          (run-at-time
-           "1 sec" nil 'delete-windows-on
-           (get-buffer-create buf))
-          (message "No Compilation Errors!")))))
+;; (setq compilation-finish-function
+;; ;; (add-hook! 'compilation-finish-functions  <-- don't know why doom-emacs doesn't like this
+;;   (lambda (buf str)
+;;     (if (null (string-match ".*exited abnormally.*" str))
+;;         ;;no errors, make the compilation window go away in a few seconds
+;;         (progn
+;;           (run-at-time
+;;            "1 sec" nil 'delete-windows-on
+;;            (get-buffer-create buf))
+;;           (message "No Compilation Errors!")))))
 
 ;; and when losing focus, enter normal mode and go ahead and save
 (add-hook! '(doom-switch-window-hook
@@ -83,11 +89,36 @@
 
 ;; org specific things:
 
+
 ;; this keymap is similar to the evil default "z i" for org-toggle-inline-images
 (map! :mode org-mode :n "z p" 'org-toggle-latex-fragment)
 
+;; prefer the unshifted semicolon for Ex commands
+(map! :leader ";" 'execute-extended-command)
+(map! :leader ":" 'eval-expression)
 
 (after! org
+
+  ;; Email link handlink for message:// links
+  (setq thunderbird-program "/usr/bin/thunderbird")
+  (defun org-message-thunderlink-open (slash-message-id)
+    "Handler for org-link-set-parameters that converts a standard message:// link into
+   a thunderlink and then invokes thunderbird."
+    ;; remove any / at the start of slash-message-id to create real message-id
+    (let ((message-id
+           (replace-regexp-in-string (rx bos (* "/"))
+                                     ""
+                                     slash-message-id)))
+      (start-process
+       (concat "thunderlink: " message-id)
+       nil
+       thunderbird-program
+       "-thunderlink"
+       (concat "thunderlink://messageid=" message-id)
+       )))
+  ;; on message://aoeu link, this will call handler with //aoeu
+  (org-link-set-parameters "message" :follow #'org-message-thunderlink-open)
+
   (setq org-capture-templates '(("t" "Todo [inbox]" entry
                                (file "~/org/inbox.org")
                                "* TODO %i%?" :prepend t)
